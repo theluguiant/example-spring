1db7b9e9040ea884dd69d0388ce54818440af48c
Garcia <fredy.garcia@accenture.com>
2019-02-28 10:02:35 -0500

    se agregan datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clients(active, active_at, create_at, email, name, last) values(TRUE, '2019-02-28', '2019-02-28', 'prueba1@gmail.com', 'prueba1', 'aprueba1' );
+INSERT INTO clients(active, active_at, create_at, email, name, last) values(TRUE, '2019-02-28', '2019-02-28', 'prueba2@gmail.com', 'prueba2', 'aprueba2' );
+INSERT INTO clients(active, active_at, create_at, email, name, last) values(TRUE, '2019-02-28', '2019-02-28', 'prueba3@gmail.com', 'prueba3', 'aprueba3' );
+INSERT INTO clients(active, active_at, create_at, email, name, last) values(TRUE, '2019-02-28', '2019-02-28', 'prueba4@gmail.com', 'prueba4', 'aprueba4' );
+INSERT INTO clients(active, active_at, create_at, email, name, last) values(TRUE, '2019-02-28', '2019-02-28', 'prueba5@gmail.com', 'prueba5', 'aprueba5' );
